10154e2aa60d9ed043a233c0ff6754f2cf8ac32c
Bombarding <expuhloits@gmail.com>
2023-08-11 13:10:00 -0400

    add destroy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,12 @@
 variable "bucket" {
   type = string
+  default = "terraform-bucket-personal"
 }
 
 variable "region" {
   type        = string
   description = "The region in which the resources will be created"
   default     = "us-east-1"
-}
-
-variable "access_key" {
-  type        = string
-  description = "The aws development account access key"
-}
-
-variable "secret_key" {
-  type        = string
-  description = "The aws development account secret key"
 }
 
 variable "bucket_key" {
@@ -27,3 +18,6 @@ variable "cognito_pool_name" {
   type    = string
   default = "cognito-pool"
 }
+
+variable "access_key" {}
+variable "secret_key" {}
