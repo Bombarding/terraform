2b1be85ab03cf1558a3aa4fbdc8525ff9b8d02da
Bombarding <expuhloits@gmail.com>
2023-08-11 12:53:52 -0400

    update region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,7 @@ variable "bucket" {
 variable "region" {
   type        = string
   description = "The region in which the resources will be created"
+  default = "us-east-1"
 }
 
 variable "access_key" {
