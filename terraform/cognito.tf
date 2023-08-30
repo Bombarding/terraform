7548a7ff066af23fb73aac04a679dc09255d3f1d
ajsteel <22651146+Bombarding@users.noreply.github.com>
2023-08-30 12:15:41 -0400

    Update cognito pool

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -4,7 +4,27 @@ module "aws_cognito_user_pool_simple" {
   user_pool_name = var.cognito_pool_name
   tags = {
     Owner       = "Bombarding"
-    Environment = "testing"
+    Environment = "Testing"
     Terraform   = true
   }
+
+  password_policy = {
+    minimum_length                   = 10
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 90
+  }
+
+  recovery_mechanisms = [
+    {
+      name     = "verified_email"
+      priority = 1
+    },
+    {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  ]
 }
